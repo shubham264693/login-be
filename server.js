9d183ad3d9ec8e725a6db07e0ddd16d7f3c418bc
shubham264693 <shubhamkale008@gmail.com>
2025-04-29 15:15:44 +0530

    corse issue resolved

--- server.js
+++ server.js
@@ -2,19 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const users = require('./users');
+const morgan = require('morgan');
+
 
 const app = express();
 const PORT = 5000;
 
 // Middleware
 // app.use(cors());
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(bodyParser.json());
+app.use(morgan('combined'));
+
+app.use((req, res, next) => {
+  res.setHeader('Transfer-Encoding', '');
+  next();
+});
+app.use((_req, res, next) => {
+  res.header("Server", "");
+  next();
+}); 
 
 // Routes
 app.post('/login', users.login);
